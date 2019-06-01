13de408f97cfe5527c01f1e67087bf9f12a6fc6b
kuailexiaoyi <409718341@qq.com>
2019-06-01 10:41:26 +0800

    SpringCloud Eureka注册中心

---
+++ src/main/java/com/springcloud/eureka/spirngcloudeureka/SpirngcloudEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.eureka.spirngcloudeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngcloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpirngcloudEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springcloud/eureka/spirngcloudeureka/SpirngcloudEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springcloud.eureka.spirngcloudeureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpirngcloudEurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
